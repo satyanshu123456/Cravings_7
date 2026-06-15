2c51abc74ab725008d4e2dff81fe3564ebe22080
satyanshu1234567 <satyanshugautam6@gmail.com>
2026-06-15 17:12:17 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import HelpPage from "../assets/HelpPage.jpg";
 
 const Home = () => {
   return (
     <>
-      <div>Fha Fha</div>
+      <div className="relative w-full h-auto">
+        <img src={HelpPage} alt="Help Page" className="w-full h-auto" />
+
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-white bg-black/40 rounded-lg p-4">
+          <h1 className="text-4xl font-bold mb-2">Welcome to Cravings</h1>
+          <p className="text-lg text-center">
+            Your Favorite Food, Delivered Fast
+          </p>
+          <div className="flex gap-4 mt-4">
+            <button className="mt-4 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg">
+              SignUp
+            </button>
+
+            <button className="mt-4 px-6 py-2 bg-blue-600  text-white font-semibold rounded-lg">
+              Register Now
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
